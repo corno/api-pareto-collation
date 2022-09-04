72cbb4ac47a5134bdd060ada47ac87593c99f2de
corno <corno.astn.a@schraverus.com>
2022-09-04 10:56:15 +0200

    .

--- pub/src/interface/tbd.ts
+++ pub/src/interface/tbd.ts
@@ -1,7 +1,20 @@
+import * as pt from "pareto-core-types"
 
-export type IsYinBeforeYang = (
+export type FIsYinBeforeYang = (
     $: {
         yin: string,
         yang: string,
     }
 ) => boolean
+
+export type XSortedForEach = <T>(
+    $: pt.Dictionary<T>,
+    $i: ($: T) => void
+) => void
+
+
+export type XCreateSortedForEach = (
+    $d: {
+        isYinBeforeYang: FIsYinBeforeYang
+    }
+) => XSortedForEach
