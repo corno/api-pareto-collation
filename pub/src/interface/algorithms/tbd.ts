0d9f25660514bae7ca02a9dd51537c4d3589e043
corno <corno.astn.a@schraverus.com>
2022-09-06 15:26:14 +0200

    .

--- pub/src/interface/algorithms/tbd.ts
+++ pub/src/interface/algorithms/tbd.ts
@@ -2,14 +2,17 @@ import * as pt from "pareto-core-types"
 
 export type FIsYinBeforeYang = (
     $: {
-     readonly  "yin": string,
-      readonly  "yang": string,
+        readonly "yin": string,
+        readonly "yang": string,
     }
 ) => boolean
 
 export type PSortedForEach = <T>(
     $: pt.Dictionary<T>,
-    $i: ($: T, key: string) => void
+    $i: ($: {
+        readonly "entry": T,
+        readonly "key": string
+    }) => void
 ) => void
 
 
